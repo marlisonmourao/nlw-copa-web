cfc7c66d6fc196b1bd0359554a1a847c75ea435d
marlisonmourao <bentesmourao@gmail.com>
2022-11-04 01:49:07 -0400

    full application

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,10 +9,12 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,700&display=swap" rel="stylesheet"/>
       </Head>
 
-      <body>
+      <body className='bg-gray-900 bg-app bg-no-repeat bg-cover'>
         <Main/>
         <NextScript />
       </body>
+
+      
     </Html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif'
+      },
+
+      backgroundImage: {
+        app: 'url(/bg-background.png)'
+      },
+
+      colors: {
+        ignite: {
+          500: '#129E57',
+        },
+        gray: {
+          100: '#E1E1E6',
+          300: '#8D8D99',
+          600: '#323238',
+          800: '#202024',
+          900: '#121214'
+        },
+        yellow: {
+          500: '#F7DD43',
+          700: '#E5CD3D'
+        }
+      }
+    },
   },
   plugins: [],
 }
